e92f2ba72db3dd7a1db9b6233327dbe34833f44b
ambalabanov <a.balabanov@icloud.com>
2020-11-11 17:42:26 +0500

    fix network,del cdrom

--- rhel.sh
+++ rhel.sh
@@ -3,7 +3,6 @@ qemu-system-x86_64 \
  -accel hvf \
  -smp 2 \
  -m 2048 \
- -cdrom rhel-8.3-x86_64-boot.iso \
  -hda rhel-8.3-x86_64-kvm.qcow2 \
  -netdev user,id=mynet0,hostfwd=tcp:127.0.0.1:2202-:22 \
  -device e1000,netdev=mynet0 \
--- ubuntu.sh
+++ ubuntu.sh
@@ -3,8 +3,7 @@ qemu-system-x86_64 \
 -accel hvf \
 -smp 2 \
 -m 2048 \
--cdrom ubuntu-16.04.7-server-amd64.iso \
--hda ubuntu.qcow2 \
+-hda focal-server-cloudimg-amd64-disk-kvm.qcow2 \
 -netdev user,id=mynet0,hostfwd=tcp:127.0.0.1:2203-:22 \
--device e1000,netdev=mynet0 \
+-device virtio-net-pci,netdev=mynet0 \
 -nographic
