12520570477aa1c0b1f682e32d639d68aa5978d2
ambalabanov <a.balabanov@icloud.com>
2021-06-03 23:10:05 +0500

    update version

--- freebsd.sh
+++ freebsd.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 qemu-system-x86_64 -m 2048 \
  -accel hvf \
- -hda FreeBSD-12.2-RELEASE-arm64-aarch64.qcow2 \
+ -hda FreeBSD-13.0-RELEASE-amd64.qcow2 \
  -netdev user,id=mynet0,hostfwd=tcp:127.0.0.1:2201-:22 \
  -device e1000,netdev=mynet0 \
  -nographic 
--- rhel.sh
+++ rhel.sh
@@ -3,7 +3,7 @@ qemu-system-x86_64 \
  -accel hvf \
  -smp 2 \
  -m 2048 \
- -hda rhel-8.3-x86_64-kvm.qcow2 \
+ -hda rhel-8.4-x86_64-kvm.qcow2 \
  -netdev user,id=mynet0,hostfwd=tcp:127.0.0.1:2202-:22 \
  -device e1000,netdev=mynet0 \
  -nographic
