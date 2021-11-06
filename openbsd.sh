82cba78fef12e94e13c2953ca5219ae32ac2caf9
Andrey Balabanov <a.balabanov@icloud.com>
2021-11-06 13:51:00 +0000

    kvm & 7.0

--- openbsd.sh
+++ openbsd.sh
@@ -1,7 +1,8 @@
 #/bin/bash
-qemu-system-x86_64 -m 2048 \
- -accel hvf \
- -cdrom cd69.iso \
+qemu-system-x86_64 \
+ -m 2048 \
+ -accel kvm \
+ -cdrom cd70.iso \
  -hda openbsd.qcow2 \
  -netdev user,id=mynet0,hostfwd=tcp:127.0.0.1:2202-:22 \
  -device e1000,netdev=mynet0 \
